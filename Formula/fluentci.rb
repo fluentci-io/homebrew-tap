ff8336e1a5f12197fb99e9d85406ff749cc5275a
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-10-18 14:52:01 +0000

    update Formula

--- Formula/fluentci.rb
+++ Formula/fluentci.rb
@@ -1,22 +1,22 @@
 class Fluentci < Formula
   desc "Command line interface for Fluent CI/CD"
   homepage "https://github.com/fluentci-io/fluentci"
-  url "https://github.com/fluentci-io/fluentci/releases/download/v0.6.6/fluentci_v0.6.6_aarch64-apple-darwin.tar.gz"
-  sha256 "81867ffe6f8b2f6a3af13eb103240d46c6a08cf34834da8cb48b749a7b9aef9c"
+  url "https://github.com/fluentci-io/fluentci/releases/download/v0.6.7/fluentci_v0.6.7_aarch64-apple-darwin.tar.gz"
+  sha256 "d7e1d64719115959e544f7f45e91b481ea4478318d3300c613a20715c0480fcf"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/fluentci-io/fluentci/releases/download/v0.6.6/fluentci_v0.6.6_x86_64-apple-darwin.tar.gz"
-    sha256 "23c958ab661bbffba403ee9cc8545e837bf91c0941f6c728b143d5503978de19"
+    url "https://github.com/fluentci-io/fluentci/releases/download/v0.6.7/fluentci_v0.6.7_x86_64-apple-darwin.tar.gz"
+    sha256 "d72981dcb32f247e85deabac7bd6755311517f16300a4ed85b5dc466bd4560a8"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/fluentci-io/fluentci/releases/download/v0.6.6/fluentci_v0.6.6_aarch64-apple-darwin.tar.gz"
-    sha256 "81867ffe6f8b2f6a3af13eb103240d46c6a08cf34834da8cb48b749a7b9aef9c"
+    url "https://github.com/fluentci-io/fluentci/releases/download/v0.6.7/fluentci_v0.6.7_aarch64-apple-darwin.tar.gz"
+    sha256 "d7e1d64719115959e544f7f45e91b481ea4478318d3300c613a20715c0480fcf"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/fluentci-io/fluentci/releases/download/v0.6.6/fluentci_v0.6.6_x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "a4758123d9b8e22f3696bb65ad8df5ac1cb7c86534ab9666ba6799e334c36d0e"
+    url "https://github.com/fluentci-io/fluentci/releases/download/v0.6.7/fluentci_v0.6.7_x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "72fb499b8de39d77d2961be1f487f2cbf7ae0a9ffe957aebac81892afc511880"
   end
 
   depends_on "deno"
