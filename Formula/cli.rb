93f71ad2eb39cff5c896e1904f668568fef31d52
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-01-22 14:18:19 +0000

    update fluentci Formula

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -1,22 +1,22 @@
 class Cli < Formula
   desc "Command line interface for Fluent CI/CD"
   homepage "https://github.com/fluentci-io/fluentci"
-  url "https://github.com/fluentci-io/fluentci/releases/download/v0.10.7/fluentci_v0.10.7_aarch64-apple-darwin.tar.gz"
-  sha256 "78ba3f1601034a3f8cdc5c47f0a70caf2c99700e9e5067bd19873beee2657c55"
+  url "https://github.com/fluentci-io/fluentci/releases/download/v0.11.0/fluentci_v0.11.0_aarch64-apple-darwin.tar.gz"
+  sha256 "7fa133f419ed8324fbecdba45331e74516479c7e629cb64057ce65c30799634f"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/fluentci-io/fluentci/releases/download/v0.10.7/fluentci_v0.10.7_x86_64-apple-darwin.tar.gz"
-    sha256 "18e6017296fc4e2a6549a05c5d1f82efc056465abb956fdae2d3b86f2bc1d66e"
+    url "https://github.com/fluentci-io/fluentci/releases/download/v0.11.0/fluentci_v0.11.0_x86_64-apple-darwin.tar.gz"
+    sha256 "8046986cb521b0156f6bb8f3a8779f3bdbef14a6ae1a737629ca3b108c4f6d0d"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/fluentci-io/fluentci/releases/download/v0.10.7/fluentci_v0.10.7_aarch64-apple-darwin.tar.gz"
-    sha256 "78ba3f1601034a3f8cdc5c47f0a70caf2c99700e9e5067bd19873beee2657c55"
+    url "https://github.com/fluentci-io/fluentci/releases/download/v0.11.0/fluentci_v0.11.0_aarch64-apple-darwin.tar.gz"
+    sha256 "7fa133f419ed8324fbecdba45331e74516479c7e629cb64057ce65c30799634f"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/fluentci-io/fluentci/releases/download/v0.10.7/fluentci_v0.10.7_x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "9739ea06e9a0b051d4d9dea243f46b353c4a89467d8ce4e6e57aed4031a5ce93"
+    url "https://github.com/fluentci-io/fluentci/releases/download/v0.11.0/fluentci_v0.11.0_x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "f3d474b89569fb44ad8112c61f5d1393cfd932b7ff39d82f0b1ecf84bb91306e"
   end
 
   depends_on "deno"
